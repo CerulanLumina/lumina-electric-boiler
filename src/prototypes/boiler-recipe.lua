10087d08156c51ddc7865644e66ee3061ff3361d
CerulanLumina <CerulanLumina@users.noreply.github.com>
2023-05-11 18:14:34 -0400

    Disable recipes by default

--- src/prototypes/boiler-recipe.lua
+++ src/prototypes/boiler-recipe.lua
@@ -1,35 +1,35 @@
+local abstract_recipe = {
+    type = "recipe",
+    enabled = false,
+}
+
+local function recipe(name, overrides)
+    return util.merge({abstract_recipe, overrides, { name = name, result = name }})
+end
+
 return {
-    [1] = {
-        name = "lumina-electric-boiler-1",
-        type = "recipe",
+    [1] = recipe("lumina-electric-boiler-1", {
         ingredients = {
             {"steel-plate", 10},
             {"advanced-circuit", 5},
             {"boiler", 1},
         },
-        result = "lumina-electric-boiler-1",
         energy_required = 5,
-    },
-    [2] = {
-        name = "lumina-electric-boiler-2",
-        type = "recipe",
+    }),
+    [2] = recipe("lumina-electric-boiler-2", {
         ingredients = {
             {"steel-plate", 10},
             {"processing-unit", 5},
             {"lumina-electric-boiler-1", 1},
         },
-        result = "lumina-electric-boiler-2",
         energy_required = 5,
-    },
-    [3] = {
-        name = "lumina-electric-boiler-3",
-        type = "recipe",
+    }),
+    [3] = recipe("lumina-electric-boiler-3", {
         ingredients = {
             {"steel-plate", 10},
             {"processing-unit", 15},
             {"lumina-electric-boiler-2", 1},
         },
-        result = "lumina-electric-boiler-3",
         energy_required = 5,
-    },
+    }),
 }
